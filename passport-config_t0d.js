2d0ada5ca8aaf32109f6360bd62aff6af13a851a
tsalm0n <tsalm0n@twc.com>
2019-08-23 02:55:49 -0400

    ...

--- passport-config_t0d.js
+++ passport-config_t0d.js
@@ -1,5 +1,5 @@
 module.exports = {
   clientID: '57ca0c684f66556eb226',
   clientSecret: 'd4736813709c7b1e4d06fb3fa013fc4f072213cf',
-  callbackURL: "https://rbcat.herokuapp.com/auth_t0d"
+  callbackURL: "https://rbcat.herokuapp.com/login/auth_t0d"
 }
