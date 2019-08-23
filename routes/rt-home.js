49f2f5835e263c0b0d3fcb0d127a72ec2356f133
tsalm0n <tsalm0n@twc.com>
2019-08-23 10:43:06 -0400

    routing

--- routes/rt-home.js
+++ routes/rt-home.js
@@ -3,18 +3,24 @@ var router = express.Router();
 
 const passport = require('passport');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   res.render('vw-home', {
+//     title: 'Catalog Tracker App'
+//   });
+// });
+
+router.get('/', passport.authenticate('github'));
+
+router.get('/auth_t0d', function (req, res, next) {
+  passport.authenticate('github', {
+    // successRedirect: '/',
+    failureRedirect: '/loginFailed'
+  })
   res.render('vw-home', {
     title: 'Catalog Tracker App'
   });
-});
+})
 
-// router.get('/', passport.authenticate('github'));
-
-// router.get('/auth_t0d', passport.authenticate('github', {
-//   successRedirect: '/home',
-//   failureRedirect: '/loginFailed'
-// }));
 
 module.exports = router;
